58c71fa681462650666cf816b5537036b3ab82b9
Kakarot <JamesRuiz@gatech.edu>
2016-04-10 03:16:20 -0400

    Changed Query to obtain codes

--- FHIR-Creator/FHIR-Creator/ReadFhir.cs
+++ FHIR-Creator/FHIR-Creator/ReadFhir.cs
@@ -26,15 +26,17 @@ namespace FHIR_Creator
             if (crud == "Read")
             {
                 var allergyIntolerance = new AllergyIntolerance(url.Trim());
-               // var allergyIntolerance = new AllergyIntolerance("http://fhirtest.uhn.ca/baseDstu2/");
+                // var allergyIntolerance = new AllergyIntolerance("http://fhirtest.uhn.ca/baseDstu2/");
                 //var patientID = 6140; //Patient ID for FHIR Server
-                
-                var medications = new List<string>() { "hydrocodone", "aspirin" }; //medications the patient is taking
-                var response = allergyIntolerance.GetListOfMedicationAllergies(patientID, medications).ToList();
-                foreach(string r in response)
+
+                // var medications = new List<string>() { "hydrocodone", "aspirin" }; //medications the patient is taking
+                //var response = allergyIntolerance.GetListOfMedicationAllergies(patientID, medications).ToList();
+                var response = allergyIntolerance.GetPatientsKnownAllergies(patientID);
+                var medicationCodes = response.Keys;     
+                foreach (string m in medicationCodes)
                 {
-                    result += r;
-                }             
+                    result += m;
+                }  
             }
             return result;
         }
